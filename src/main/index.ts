022a9c8f563c23761b9ca2b4b0e132271881f414
TheReal-Flo <florian.reintgen@gmail.com>
2025-02-21 12:52:49 +0100

    Rework from webviews to browserviews + simple search ch support

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,15 +5,38 @@ import {
   ipcMain,
   globalShortcut,
   session,
-  Menu // import Menu from electron
+  Menu, // import Menu from electron
+  BrowserView
 } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 import { ElectronBlocker } from '@ghostery/adblocker-electron'
 
+interface Windows {
+  [key: string]: BrowserWindow
+}
+
+interface Tabs {
+  [key: string]: BrowserView
+}
+
+const sidebarState = {}
+
+const windows: Windows = {}
+const tabs: Tabs = {}
+
+function broadcast(channel, ...args): void {
+  BrowserWindow.getAllWindows().forEach((win) => {
+    win.webContents.send(channel, ...args)
+  })
+}
+
 // Create new windows on demand.
 function createWindow(): void {
+  // Generate a unique ID for the new window.
+  const windowId = Math.random().toString(36).substr(2, 9)
+
   const window = new BrowserWindow({
     width: 900,
     height: 670,
@@ -23,10 +46,11 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false,
-      webviewTag: true
+      sandbox: false
     }
   })
+
+  sidebarState[window.id] = false
 
   window.on('ready-to-show', () => {
     window.show()
@@ -44,6 +68,12 @@ function createWindow(): void {
   } else {
     window.loadFile(join(__dirname, '../renderer/index.html'))
   }
+
+  windows[windowId] = window
+  // Handle window close.
+  window.on('closed', () => {
+    delete windows[windowId]
+  })
 }
 
 // When the app is ready, set everything up.
@@ -79,6 +109,93 @@ app.whenReady().then(() => {
   // Handle renderer requests for a new window.
   ipcMain.on('new-window', () => {
     createWindow()
+  })
+
+  ipcMain.on('sidebar-off', () => {
+    const focusedWindow = BrowserWindow.getFocusedWindow()
+    sidebarState[focusedWindow?.id || 0] = false
+
+    focusedWindow?.getBrowserView()?.setBounds({
+      x: 0,
+      y: 50,
+      width: focusedWindow?.getBounds().width || 0,
+      height: (focusedWindow?.getBounds().height || 0) - 50
+    })
+  })
+
+  ipcMain.on('sidebar-on', () => {
+    const focusedWindow = BrowserWindow.getFocusedWindow()
+    sidebarState[focusedWindow?.id || 0] = true
+
+    focusedWindow?.getBrowserView()?.setBounds({
+      x: 250,
+      y: 50,
+      width: (focusedWindow?.getBounds().width || 0) - 250,
+      height: (focusedWindow?.getBounds().height || 0) - 50
+    })
+  })
+
+  ipcMain.on('request-tab', (_e, data) => {
+    const focusedWindow = BrowserWindow.getFocusedWindow()
+    if (focusedWindow) focusedWindow.setBrowserView(tabs[data])
+    tabs[data].setAutoResize({
+      width: true,
+      height: true
+    })
+    tabs[data].setBounds({
+      x: sidebarState[focusedWindow?.id || 0] ? 250 : 0,
+      y: 50,
+      width:
+        (focusedWindow?.getBounds().width || 0) - (sidebarState[focusedWindow?.id || 0] ? 250 : 0),
+      height: (focusedWindow?.getBounds().height || 0) - 50
+    })
+
+    focusedWindow?.webContents.send(
+      'tab-data',
+      JSON.stringify({
+        id: data,
+        url: tabs[data].webContents.getURL(),
+        title: tabs[data].webContents.getTitle()
+      })
+    )
+  })
+
+  ipcMain.on('navigate-to', (_e, _data) => {
+    const data = JSON.parse(_data)
+
+    tabs[data.tabId].webContents.loadURL(data.url)
+  })
+
+  ipcMain.on('new-tab', (_e, _data) => {
+    const tabId = Math.random().toString(36).substr(2, 9)
+
+    console.log(`New Tab with id ${tabId} has been created.`)
+
+    const focusedWindow = BrowserWindow.getFocusedWindow()
+
+    const view = new BrowserView()
+    view.webContents.loadURL(_data)
+
+    view.webContents.on('page-title-updated', (_e, title) => {
+      broadcast(
+        'tab-data',
+        JSON.stringify({
+          id: tabId,
+          url: view.webContents.getURL(),
+          title
+        })
+      )
+    })
+
+    tabs[tabId] = view
+    focusedWindow?.webContents.send(
+      'tab-ready',
+      JSON.stringify({
+        id: tabId,
+        url: view.webContents.getURL() || '',
+        title: view.webContents.getTitle() || ''
+      })
+    )
   })
 
   // Use the event sender's window for close, minimize, and maximize.
